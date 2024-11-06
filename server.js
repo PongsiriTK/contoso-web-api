132ecabcc76f361ea2c42d80f06ca7ececb3aa13
pongsiri-ais <pongsiro@ais.co.th>
2024-11-06 08:39:58 +0000

    remove cors

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.get('/', async (req, res) => {
     res.send('Hello Muay Thai - 1.0.0')
 })
 
-app.use(cors({
-    origin: ['https://4606-172-191-205-199.ngrok-free.app']
-  }))
+// app.use(cors({
+//     origin: ['https://4606-172-191-205-199.ngrok-free.app']
+//   }))
 
 app.listen(PORT, () => {
     console.log(`Listening on http://${HOST}:${PORT}`)
